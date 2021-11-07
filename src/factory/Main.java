75fd79c3157ad655ac917e65d235f0158ae0178b
Oier Elola <oelola001@ikasle.ehu.eus>
2021-11-07 11:50:30 +0100

    aldaketa minimoa

--- src/factory/Main.java
+++ src/factory/Main.java
@@ -10,10 +10,10 @@ import observer.PacientObserverGUI;
 public class Main {
 
 	public static void main(String[] args) {
-		/*Covid19Pacient p1=new Covid19Pacient("aitor", 35,new SymptomSortzailea());
-		new PacientSymptomGUI(p1);
+		Covid19Pacient p1=new Covid19Pacient("aitor", 35,new SymptomSortzailea());
+		PacientSymptomGUI p1gui=new PacientSymptomGUI(p1);
+		
 		Medicament m=new Medicament("Ibuprofeno", new SymptomSortzailea());
 		MedicalGUI mgui=new MedicalGUI(m);
-		//a*/
 		}
 }
